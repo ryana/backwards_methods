5674fc73dfc550d7b66c2a623b181b5d1ca96f6b
Ryan Angilly <ryan@angilly.com>
2011-03-02 15:22:30 -0700

    whitespace :)

--- lib/backwards_methods.rb
+++ lib/backwards_methods.rb
@@ -15,12 +15,12 @@ class Object
       :private_instance_methods].each do |n|
       if respond_to?(n)
         define_method "#{n}_with_indifference" do |*args, &block|
-        BackwardsMethods::IndifferentArray.new(send("#{n}_without_indifference", *args, &block))
-        end
+         BackwardsMethods::IndifferentArray.new(send("#{n}_without_indifference", *args, &block))
+          end
         alias :"#{n}_without_indifference" :"#{n}"
         alias :"#{n}" :"#{n}_with_indifference"
       end
-      end
+    end
   end
 
   [:methods].each do |n|
